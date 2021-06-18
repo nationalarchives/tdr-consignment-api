982cd707c1b7b316284e02e74cdd328ccfc1ff28
techncl <nigel.lutale@nationalarchives.gov.uk>
2021-06-18 11:11:39 +0100

    Add repo test to check is all consignment statuses are returned

--- src/test/scala/uk/gov/nationalarchives/tdr/api/db/repository/ConsignmentStatusRepositorySpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/db/repository/ConsignmentStatusRepositorySpec.scala
@@ -46,6 +46,32 @@ class ConsignmentStatusRepositorySpec extends AnyFlatSpec with TestDatabase with
     consignmentStatus should be(empty)
   }
 
+  "getConsignmentStatus" should "return all consignment statuses for a consignment" in {
+    val db = DbConnection.db
+    val consignmentStatusRepository = new ConsignmentStatusRepository(db)
+    val consignmentId = UUID.fromString("b8271ba9-9ef4-4584-b074-5a48b2a34cec")
+    val userId = UUID.fromString("aee2d1a9-e1db-43a0-9fd6-a6c342bb187b")
+    val statusType1 = "TransferAgreement"
+    val statusValue1 = "Complete"
+    val statusType2 = "Upload"
+    val statusValue2 = "Complete"
+    val statusType3 = "Export"
+    val statusValue3 = "InProgress"
+
+    TestUtils.createConsignment(consignmentId, userId)
+    TestUtils.createConsignmentStatus(consignmentId, statusType1, statusValue1)
+    TestUtils.createConsignmentStatus(consignmentId, statusType2, statusValue2)
+    TestUtils.createConsignmentStatus(consignmentId, statusType3, statusValue3)
+
+    val consignmentStatuses = consignmentStatusRepository.getConsignmentStatus(consignmentId).futureValue
+
+    consignmentStatuses.length should be(3)
+
+    consignmentStatuses(0).statustype should be(statusType1)
+    consignmentStatuses(1).statustype should be(statusType2)
+    consignmentStatuses(2).statustype should be(statusType3)
+  }
+
   "updateConsignmentStatus" should "update a consignments' status value to 'completed'" in {
     val db = DbConnection.db
     val consignmentStatusRepository = new ConsignmentStatusRepository(db)
