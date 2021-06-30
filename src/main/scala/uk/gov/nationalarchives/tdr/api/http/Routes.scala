3209d73d3db183c85c5978b3bf63df15966349f8
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-06-30 15:14:53 +0100

    Add an exception handler.
    This gets the original posted entity and parses it to get the field name
    from the graphql query which it will return in the error message.

--- src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
@@ -2,21 +2,25 @@ package uk.gov.nationalarchives.tdr.api.http
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes.InternalServerError
 import akka.http.scaladsl.model.headers.{Origin, `Strict-Transport-Security`}
-import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.Directives.{entity, _}
 import akka.http.scaladsl.server.RouteResult.{Complete, Rejected}
 import akka.http.scaladsl.server.directives.{Credentials, DebuggingDirectives, LoggingMagnet}
-import akka.http.scaladsl.server.{Directive0, Route, RouteResult}
+import akka.http.scaladsl.server.{Directive0, ExceptionHandler, Route, RouteResult}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
 import com.typesafe.config._
 import com.typesafe.scalalogging.Logger
-import spray.json.JsValue
+import sangria.ast.{Field, OperationDefinition}
+import sangria.parser.QueryParser
+import spray.json.{JsObject, JsString, JsValue}
 import uk.gov.nationalarchives.tdr.api.auth.AuthorisationException
 import uk.gov.nationalarchives.tdr.keycloak.{KeycloakUtils, TdrKeycloakDeployment, Token}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 class Routes(val config: Config) extends Cors {
 
@@ -29,6 +33,33 @@ class Routes(val config: Config) extends Cors {
   val ttlSeconds: Int = 3600
   val transportSecurityMaxAge = 31536000
   implicit val keycloakDeployment: TdrKeycloakDeployment = TdrKeycloakDeployment(url, "tdr", ttlSeconds)
+
+  val exceptionHandler: ExceptionHandler =
+    ExceptionHandler {
+      case ex: Throwable =>
+        entity(as[JsValue]) { requestJson =>
+          val JsObject(fields) = requestJson
+          val JsString(query) = fields("query")
+
+          QueryParser.parse(query) match {
+            case Failure(parseException) =>
+              logger.error(s"Error parsing entity $requestJson", parseException)
+              complete(HttpResponse(InternalServerError, entity = parseException.getMessage))
+            case Success(document) =>
+              val fieldName = document.definitions.headOption.flatMap {
+                case operationDefinition: OperationDefinition => operationDefinition.selections.headOption.map {
+                  case field: Field => field.outputName
+                  case _ => None
+                }
+                case _ => None
+              }.getOrElse("Unknown field")
+
+              val errorMessage = s"Request with field $fieldName failed"
+              logger.error(errorMessage, ex)
+              complete(HttpResponse(InternalServerError, entity = errorMessage))
+          }
+        }
+    }
 
   // We return None rather than a failed future because we're following the async authenticator docs
   // https://doc.akka.io/docs/akka-http/10.0/routing-dsl/directives/security-directives/authenticateOAuth2Async.html
@@ -60,19 +91,21 @@ class Routes(val config: Config) extends Cors {
     DebuggingDirectives.logRequestResult(LoggingMagnet(_ => logNon200Response))
   }
 
-  val route: Route = logging {
-    optionalHeaderValueByType[Origin](()) { originHeader =>
-      corsHandler((post & path("graphql")) {
-        authenticateOAuth2Async("tdr", tokenAuthenticator) { accessToken =>
-          respondWithHeader(`Strict-Transport-Security`(transportSecurityMaxAge, includeSubDomains = true)) {
-            entity(as[JsValue]) { requestJson =>
-              GraphQLServer.endpoint(requestJson, accessToken)
+  val route: Route = handleExceptions(exceptionHandler) {
+    logging {
+      optionalHeaderValueByType[Origin](()) { originHeader =>
+        corsHandler((post & path("graphql")) {
+          authenticateOAuth2Async("tdr", tokenAuthenticator) { accessToken =>
+            respondWithHeader(`Strict-Transport-Security`(transportSecurityMaxAge, includeSubDomains = true)) {
+              entity(as[JsValue]) { requestJson =>
+                GraphQLServer.endpoint(requestJson, accessToken)
+              }
             }
           }
-        }
-      }, originHeader)
-    } ~ (get & path("healthcheck")) {
-      complete(StatusCodes.OK)
+        }, originHeader)
+      } ~ (get & path("healthcheck")) {
+        complete(StatusCodes.OK)
+      }
     }
   }
 }
