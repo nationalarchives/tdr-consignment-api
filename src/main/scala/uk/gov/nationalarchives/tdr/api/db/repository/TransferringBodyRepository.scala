52c713008e17f131424964d49171e2d1175a7b40
techncl <nigel.lutale@nationalarchives.gov.uk>
2021-07-21 18:14:28 +0100

    Add a global implicit to TransferringBodyRepository file rather than passing to each method

--- src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/TransferringBodyRepository.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/TransferringBodyRepository.scala
@@ -1,15 +1,15 @@
 package uk.gov.nationalarchives.tdr.api.db.repository
 
 import java.util.UUID
-
 import slick.jdbc.PostgresProfile.api._
 import uk.gov.nationalarchives.Tables.{Body, BodyRow, Series}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 
 class TransferringBodyRepository(db: Database) {
 
-  def getTransferringBody(seriesId: UUID)(implicit executionContext: ExecutionContext): Future[BodyRow] = {
+  def getTransferringBody(seriesId: UUID): Future[BodyRow] = {
     val query = for {
       (body, _) <- Body.join(Series).on(_.bodyid === _.bodyid).filter(_._2.seriesid === seriesId)
     } yield body
@@ -17,7 +17,7 @@ class TransferringBodyRepository(db: Database) {
     db.run(query.result).map(body => body.head)
   }
 
-  def dbHasTransferringBodies(implicit executionContext: ExecutionContext): Future[Boolean] = {
+  def dbHasTransferringBodies: Future[Boolean] = {
     val query = Body.length
     db.run(query.result).map(_ > 0)
   }
