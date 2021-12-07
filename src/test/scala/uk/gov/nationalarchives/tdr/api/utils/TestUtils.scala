65e9270b28131034252ff288a51a791813edc4fe
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-12-07 13:42:19 +0000

    Add a folder to the file table for the consignmen tests.
    This makes sure that we're getting the expected results even when there
    are two entries in the file table for a single file.

--- src/test/scala/uk/gov/nationalarchives/tdr/api/utils/TestUtils.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/utils/TestUtils.scala
@@ -3,7 +3,6 @@ package uk.gov.nationalarchives.tdr.api.utils
 import java.sql.{PreparedStatement, ResultSet, Timestamp}
 import java.time.Instant
 import java.util.UUID
-
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import akka.http.scaladsl.unmarshalling.{FromResponseUnmarshaller, Unmarshaller}
 import akka.stream.Materializer
@@ -12,6 +11,7 @@ import com.tngtech.keycloakmock.api.TokenConfig.aTokenConfig
 import io.circe.Decoder
 import io.circe.parser.decode
 import uk.gov.nationalarchives.tdr.api.db.DbConnection
+import uk.gov.nationalarchives.tdr.api.model.file.FileType
 import uk.gov.nationalarchives.tdr.api.service.FileMetadataService._
 import uk.gov.nationalarchives.tdr.api.service.TransferAgreementService._
 
@@ -189,13 +189,14 @@ object TestUtils {
 
   //scalastyle:on magic.number
 
-  def createFile(fileId: UUID, consignmentId: UUID): Unit = {
-    val sql = s"INSERT INTO File (FileId, ConsignmentId, UserId, Datetime) VALUES (?, ?, ?, ?)"
+  def createFile(fileId: UUID, consignmentId: UUID, fileType: String = FileType.fileTypeIdentifier): Unit = {
+    val sql = s"INSERT INTO File (FileId, ConsignmentId, UserId, Datetime, FileType) VALUES (?, ?, ?, ?, ?)"
     val ps: PreparedStatement = DbConnection.db.source.createConnection().prepareStatement(sql)
     ps.setString(1, fileId.toString)
     ps.setString(2, consignmentId.toString)
     ps.setString(3, userId.toString)
     ps.setTimestamp(4, Timestamp.from(FixedTimeSource.now))
+    ps.setString(5, fileType)
     ps.executeUpdate()
   }
 
