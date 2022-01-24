b6736f2833dcd8f217d02fe0a9ff767627206019
techncl <nigel.lutale@nationalarchives.gov.uk>
2022-01-24 17:43:06 +0000

    Add addTransferAgreement endpoint back temporarily to prevent e2e tests from failing

--- src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/TransferAgreementFields.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/TransferAgreementFields.scala
@@ -13,6 +13,22 @@ import uk.gov.nationalarchives.tdr.api.graphql.fields.FieldTypes._
 
 object TransferAgreementFields {
 
+  case class TransferAgreement(consignmentId: UUID,
+                               allPublicRecords: Boolean,
+                               allCrownCopyright: Boolean,
+                               allEnglish: Boolean,
+                               appraisalSelectionSignedOff: Boolean,
+                               initialOpenRecords: Boolean,
+                               sensitivityReviewSignedOff: Boolean)
+
+  case class AddTransferAgreementInput(consignmentId: UUID,
+                                       allPublicRecords: Boolean,
+                                       allCrownCopyright: Boolean,
+                                       allEnglish: Boolean,
+                                       appraisalSelectionSignedOff: Boolean,
+                                       initialOpenRecords: Boolean,
+                                       sensitivityReviewSignedOff: Boolean) extends UserOwnsConsignment
+
   case class TransferAgreementNotCompliance(consignmentId: UUID,
                                             allPublicRecords: Boolean,
                                             allCrownCopyright: Boolean,
@@ -42,12 +58,21 @@ object TransferAgreementFields {
     deriveInputObjectType[AddTransferAgreementComplianceInput]()
 
   val ConsignmentIdArg: Argument[UUID] = Argument("consignmentid", UuidType)
+  val TransferAgreementType: ObjectType[Unit, TransferAgreement] = deriveObjectType[Unit, TransferAgreement]()
+  val AddTransferAgreementInputType: InputObjectType[AddTransferAgreementInput] = deriveInputObjectType[AddTransferAgreementInput]()
+
+  val TransferAgreementInputArg: Argument[AddTransferAgreementInput] = Argument("addTransferAgreementInput", AddTransferAgreementInputType)
   val TransferAgreementNotComplianceInputArg: Argument[AddTransferAgreementNotComplianceInput] =
     Argument("addTransferAgreementNotComplianceInput", AddTransferAgreementNotComplianceInputType)
   val TransferAgreementComplianceInputArg: Argument[AddTransferAgreementComplianceInput] =
     Argument("addTransferAgreementComplianceInput", AddTransferAgreementComplianceInputType)
 
   val mutationFields: List[Field[ConsignmentApiContext, Unit]] = fields[ConsignmentApiContext, Unit](
+    Field("addTransferAgreement", TransferAgreementType,
+      arguments=TransferAgreementInputArg :: Nil,
+      resolve = ctx => ctx.ctx.transferAgreementService.addTransferAgreement(ctx.arg(TransferAgreementInputArg), ctx.ctx.accessToken.userId),
+      tags=List(ValidateUserHasAccessToConsignment(TransferAgreementInputArg))
+    ),
     Field("addTransferAgreementNotCompliance", TransferAgreementNotComplianceType,
       arguments=TransferAgreementNotComplianceInputArg :: Nil,
       resolve = ctx =>
--- src/main/scala/uk/gov/nationalarchives/tdr/api/service/TransferAgreementService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/service/TransferAgreementService.scala
@@ -4,9 +4,12 @@ import uk.gov.nationalarchives.Tables._
 import uk.gov.nationalarchives.tdr.api.db.repository.{ConsignmentMetadataRepository, ConsignmentStatusRepository}
 import uk.gov.nationalarchives.tdr.api.graphql.fields.TransferAgreementFields.{
   AddTransferAgreementComplianceInput,
+  AddTransferAgreementInput,
   AddTransferAgreementNotComplianceInput,
+  TransferAgreement,
   TransferAgreementCompliance,
-  TransferAgreementNotCompliance}
+  TransferAgreementNotCompliance
+}
 import uk.gov.nationalarchives.tdr.api.service.TransferAgreementService._
 
 import java.sql.Timestamp
@@ -16,6 +19,46 @@ import scala.concurrent.{ExecutionContext, Future}
 class TransferAgreementService(consignmentMetadataRepository: ConsignmentMetadataRepository,
                                consignmentStatusRepository: ConsignmentStatusRepository,
                                uuidSource: UUIDSource, timeSource: TimeSource)(implicit val executionContext: ExecutionContext) {
+
+  def addTransferAgreement(input: AddTransferAgreementInput, userId: UUID): Future[TransferAgreement] = {
+    for {
+      transferAgreement <- consignmentMetadataRepository.addConsignmentMetadata(convertInputToPropertyRows(input, userId)).map(
+        rows => convertDbRowsToTransferAgreement(input.consignmentId, rows)
+      )
+      _ <- addTransferAgreementStatus(input.consignmentId, "Completed")
+    } yield transferAgreement
+  }
+
+  private def convertInputToPropertyRows(input: AddTransferAgreementInput, userId: UUID): Seq[ConsignmentmetadataRow] = {
+    val time = Timestamp.from(timeSource.now)
+    val consignmentId = input.consignmentId
+    Seq(
+      ConsignmentmetadataRow(
+        uuidSource.uuid, consignmentId, PublicRecordsConfirmed, input.allPublicRecords.toString, time, userId),
+      ConsignmentmetadataRow(
+        uuidSource.uuid, consignmentId, AllEnglishConfirmed, input.allEnglish.toString, time, userId),
+      ConsignmentmetadataRow(
+        uuidSource.uuid, consignmentId, AppraisalSelectionSignOffConfirmed, input.appraisalSelectionSignedOff.toString, time, userId),
+      ConsignmentmetadataRow(
+        uuidSource.uuid, consignmentId, CrownCopyrightConfirmed, input.allCrownCopyright.toString, time, userId),
+      ConsignmentmetadataRow(
+        uuidSource.uuid, consignmentId, InitialOpenRecordsConfirmed, input.initialOpenRecords.toString, time, userId),
+      ConsignmentmetadataRow(
+        uuidSource.uuid, consignmentId, SensitivityReviewSignOffConfirmed, input.sensitivityReviewSignedOff.toString, time, userId)
+    )
+  }
+
+  private def convertDbRowsToTransferAgreement(consignmentId: UUID, rows: Seq[ConsignmentmetadataRow]): TransferAgreement = {
+    val propertyNameToValue = rows.map(row => row.propertyname -> row.value.toBoolean).toMap
+    TransferAgreement(consignmentId,
+      propertyNameToValue(PublicRecordsConfirmed),
+      propertyNameToValue(CrownCopyrightConfirmed),
+      propertyNameToValue(AllEnglishConfirmed),
+      propertyNameToValue(AppraisalSelectionSignOffConfirmed),
+      propertyNameToValue(InitialOpenRecordsConfirmed),
+      propertyNameToValue(SensitivityReviewSignOffConfirmed)
+    )
+  }
 
   def addTransferAgreementNotCompliance(input: AddTransferAgreementNotComplianceInput, userId: UUID): Future[TransferAgreementNotCompliance] = {
     for {
