da2251e2f4afb7a5a01a472455e1d86af4da13b5
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-08 14:56:35 +0100

    Remove unused imports and variables

--- src/test/scala/uk/gov/nationalarchives/tdr/api/utils/TestContainerUtils.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/utils/TestContainerUtils.scala
@@ -1,14 +1,13 @@
 package uk.gov.nationalarchives.tdr.api.utils
 
 import akka.stream.alpakka.slick.scaladsl.SlickSession
-import com.dimafeng.testcontainers.scalatest.{TestContainerForAll, TestContainerForEach}
+import com.dimafeng.testcontainers.scalatest.TestContainerForEach
 import com.dimafeng.testcontainers.{ContainerDef, PostgreSQLContainer}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.testcontainers.utility.DockerImageName
 import slick.jdbc.JdbcBackend
-import uk.gov.nationalarchives.tdr.api.graphql.fields.FileMetadataFields.SHA256ServerSideChecksum
 import uk.gov.nationalarchives.tdr.api.utils.TestContainerUtils._
 import uk.gov.nationalarchives.tdr.api.utils.TestUtils._
 
@@ -20,11 +19,6 @@ trait TestContainerUtils extends AnyFlatSpec with TestContainerForEach with Befo
     username = "tdr",
     password = "password"
   )
-
-  override def afterEach(): Unit = {
-    val db = SlickSession.forConfig("consignmentapi").db
-
-  }
 
   override def afterContainersStart(containers: containerDef.Container): Unit = {
     containers match {
