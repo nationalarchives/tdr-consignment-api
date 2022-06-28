f0b6fef9ee709e42f057729e9370c2bb5da10808
techncl <nigel.lutale@nationalarchives.gov.uk>
2022-06-28 11:15:21 +0100

    Refactor 'getClosureMetadata' service method to make it easier to follow

--- src/main/scala/uk/gov/nationalarchives/tdr/api/service/CustomMetadataPropertiesService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/service/CustomMetadataPropertiesService.scala
@@ -10,59 +10,73 @@ class CustomMetadataPropertiesService(customMetadataPropertiesRepository: Custom
                                       (implicit val ec: ExecutionContext) {
 
   def getClosureMetadata: Future[Seq[CustomMetadataField]] = {
-    (for {
-      properties <- customMetadataPropertiesRepository.getClosureMetadataProperty
-      values <- customMetadataPropertiesRepository.getClosureMetadataValues
-      dependencies <- customMetadataPropertiesRepository.getClosureMetadataDependencies
-    } yield (properties, values, dependencies)).map {
+    val propertiesValuesAndDependencies: Future[(Seq[FilepropertyRow], Seq[FilepropertyvaluesRow], Seq[FilepropertydependenciesRow])] =
+      for {
+        properties <- customMetadataPropertiesRepository.getClosureMetadataProperty
+        values <- customMetadataPropertiesRepository.getClosureMetadataValues
+        dependencies <- customMetadataPropertiesRepository.getClosureMetadataDependencies
+      } yield (properties, values, dependencies)
+
+    propertiesValuesAndDependencies.map {
       case (propertiesResult, valuesResult, dependenciesResult) =>
         val values: Map[String, Seq[FilepropertyvaluesRow]] = valuesResult.groupBy(_.propertyname)
         val dependencies: Map[Int, Seq[FilepropertydependenciesRow]] = dependenciesResult.groupBy(_.groupid)
 
-        def rowsToMetadata(fp: FilepropertyRow, defaultValueOption: Option[String] = None): CustomMetadataField = {
-          val metadataValues: Seq[CustomMetadataValues] = values.getOrElse(fp.name, Nil).map(value => {
-            value.dependencies.map(groupId => {
-              val deps: Seq[CustomMetadataField] = for {
-                dep <- dependencies.getOrElse(groupId, Nil)
-                dependencyProps <- propertiesResult.find(_.name == dep.propertyname).map(fp => {
-                  rowsToMetadata(fp, dep.default)
-                })
-              } yield dependencyProps
-              CustomMetadataValues(deps.toList, value.propertyvalue)
-            }).getOrElse(CustomMetadataValues(Nil, value.propertyvalue))
-          })
-          CustomMetadataField(
-            fp.name,
-            fp.fullname,
-            fp.description,
-            getPropertyType(fp.propertytype),
-            fp.propertygroup,
-            getDataType(fp.datatype),
-            fp.editable.getOrElse(false),
-            fp.multivalue.getOrElse(false),
-            defaultValueOption,
-            metadataValues.toList
-          )
-        }
-
-        propertiesResult.map(prop => {
-          val defaultValue: Option[String] = for {
-            values <- values.get(prop.name)
-            value <- values.find(_.default.getOrElse(false))
-          } yield value.propertyvalue
-          rowsToMetadata(prop, defaultValue)
-        }).toList
+        propertiesResult.map{
+          prop => {
+            val defaultValue: Option[String] = for {
+              values <- values.get(prop.name)
+              value <- values.find(_.default.getOrElse(false))
+            } yield value.propertyvalue
+            rowsToMetadata(prop, values, dependencies, propertiesResult,  defaultValue)
+          }
+        }.toList
     }
   }
 
-  def getPropertyType(propertyType: Option[String]): PropertyType = propertyType match {
+  private def rowsToMetadata(fp: FilepropertyRow,
+                     values: Map[String, Seq[FilepropertyvaluesRow]],
+                     dependencies: Map[Int, Seq[FilepropertydependenciesRow]],
+                     propertiesResult: Seq[FilepropertyRow],
+                     defaultValueOption: Option[String] = None): CustomMetadataField = {
+
+    val metadataValues: Seq[CustomMetadataValues] = values.getOrElse(fp.name, Nil).map{
+      value =>
+        value.dependencies.map{
+          groupId => {
+            val deps: Seq[CustomMetadataField] = for {
+              dep <- dependencies.getOrElse(groupId, Nil)
+              dependencyProps <- propertiesResult
+                .find(_.name == dep.propertyname)
+                .map(fp => rowsToMetadata(fp, values, dependencies, propertiesResult, dep.default))
+            } yield dependencyProps
+            CustomMetadataValues(deps.toList, value.propertyvalue)
+          }
+        }.getOrElse(CustomMetadataValues(Nil, value.propertyvalue))
+    }
+
+    CustomMetadataField(
+      fp.name,
+      fp.fullname,
+      fp.description,
+      getPropertyType(fp.propertytype),
+      fp.propertygroup,
+      getDataType(fp.datatype),
+      fp.editable.getOrElse(false),
+      fp.multivalue.getOrElse(false),
+      defaultValueOption,
+      metadataValues.toList
+    )
+  }
+
+  private def getPropertyType(propertyType: Option[String]): PropertyType = propertyType match {
     case Some("System") => System
     case Some("Defined") => Defined
     case Some("Supplied") => Supplied
     case _ => throw new Exception(s"Invalid property type $propertyType")
   }
 
-  def getDataType(dataType: Option[String]): DataType = dataType match {
+  private def getDataType(dataType: Option[String]): DataType = dataType match {
     case Some("text") => Text
     case Some("datetime") => DateTime
     case Some("integer") => Integer
