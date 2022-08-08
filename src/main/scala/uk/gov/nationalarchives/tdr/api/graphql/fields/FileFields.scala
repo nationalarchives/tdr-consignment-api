b0ebfe92acf6addf870357d6d676bb7e0d159cd5
TomJKing <tom.king@nationalarchives.gov.uk>
2022-08-08 14:04:09 +0100

    'GetAllDescendants' query to support navigation UI

--- src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/FileFields.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/FileFields.scala
@@ -8,11 +8,12 @@ import uk.gov.nationalarchives.tdr.api.auth.ValidateUserHasAccessToConsignment
 import uk.gov.nationalarchives.tdr.api.graphql.ConsignmentApiContext
 import uk.gov.nationalarchives.tdr.api.graphql.validation.UserOwnsConsignment
 import uk.gov.nationalarchives.tdr.api.graphql.fields.FieldTypes.UuidType
+import uk.gov.nationalarchives.tdr.api.graphql.fields.ConsignmentFields.FileType
 
 import java.util.UUID
 
 object FileFields {
-  case class Files(fileIds: Seq[UUID])
+  //case class Files(fileIds: Seq[UUID])
   case class FileMatches(fileId: UUID, matchId: Long)
 
   case class ClientSideMetadataInput(originalPath: String,
@@ -24,11 +25,14 @@ object FileFields {
                                      metadataInput: List[ClientSideMetadataInput],
                                      emptyDirectories: List[String] = Nil) extends UserOwnsConsignment
 
+  case class GetAllDescendantsInput(consignmentId: UUID, parentIds: List[UUID]) extends UserOwnsConsignment
+
   implicit val MetadataInputType: InputObjectType[ClientSideMetadataInput] = deriveInputObjectType[ClientSideMetadataInput]()
   implicit val AddFileAndMetadataInputType: InputObjectType[AddFileAndMetadataInput] = deriveInputObjectType[AddFileAndMetadataInput]()
-  implicit val FileType: ObjectType[Unit, Files]  = deriveObjectType[Unit, Files]()
+  implicit val GetAllDescendantsInputType: InputObjectType[GetAllDescendantsInput] = deriveInputObjectType[GetAllDescendantsInput]()
   implicit val FileSequenceType: ObjectType[Unit, FileMatches]  = deriveObjectType[Unit, FileMatches]()
   private val FileAndMetadataInputArg = Argument("addFilesAndMetadataInput", AddFileAndMetadataInputType)
+  private val GetAllDescendantsInputArg = Argument("getAllDescendantsInput", GetAllDescendantsInputType)
 
   val mutationFields: List[Field[ConsignmentApiContext, Unit]] = fields[ConsignmentApiContext, Unit](
     Field(
@@ -39,4 +43,12 @@ object FileFields {
       tags=List(ValidateUserHasAccessToConsignment(FileAndMetadataInputArg))
     )
   )
+
+  val queryFields: List[Field[ConsignmentApiContext, Unit]] = fields[ConsignmentApiContext, Unit](
+    Field("getAllDescendants",
+      ListType(FileType),
+      arguments = GetAllDescendantsInputArg :: Nil,
+      resolve = ctx => ctx.ctx.fileService.getAllDescendants(ctx.arg(GetAllDescendantsInputArg)),
+      tags = List(ValidateUserHasAccessToConsignment(GetAllDescendantsInputArg))
+    ))
 }
--- src/test/scala/uk/gov/nationalarchives/tdr/api/routes/FileRouteSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/routes/FileRouteSpec.scala
@@ -7,9 +7,9 @@ import io.circe.generic.extras.auto._
 import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import uk.gov.nationalarchives.tdr.api.service.FileMetadataService.{clientSideProperties, staticMetadataProperties}
+import uk.gov.nationalarchives.tdr.api.utils.TestAuthUtils._
 import uk.gov.nationalarchives.tdr.api.utils.TestContainerUtils._
 import uk.gov.nationalarchives.tdr.api.utils.TestUtils._
-import uk.gov.nationalarchives.tdr.api.utils.TestAuthUtils._
 import uk.gov.nationalarchives.tdr.api.utils.{FixedUUIDSource, TestContainerUtils, TestRequest, TestUtils}
 
 import java.sql.{PreparedStatement, Types}
@@ -21,21 +21,34 @@ class FileRouteSpec extends TestContainerUtils with Matchers with TestRequest {
   case class FileNameAndPath(fileName: String, path: String)
 
   private val addFilesAndMetadataJsonFilePrefix: String = "json/addfileandmetadata_"
+  private val getAllDescendantsJsonFilePrefix: String = "json/getalldescendants_"
 
   implicit val customConfig: Configuration = Configuration.default.withDefaults
 
   case class GraphqlMutationDataFilesMetadata(data: Option[AddFilesAndMetadata], errors: List[GraphqlError] = Nil)
-
-  case class File(fileIds: Seq[UUID])
+  case class GraphqlQueryDataGetAllDescendants(data: Option[GetAllDescendants], errors: List[GraphqlError] = Nil)
 
   case class FileMatches(fileId: UUID, matchId: Long)
 
+  case class File(fileId: UUID,
+                  fileType: Option[String],
+                  fileName: Option[String],
+                  parentId: Option[UUID]
+                 )
+
+
   case class AddFilesAndMetadata(addFilesAndMetadata: List[FileMatches])
+  case class GetAllDescendants(getAllDescendants: List[File])
 
   val runTestMutationFileMetadata: (String, OAuth2BearerToken) => GraphqlMutationDataFilesMetadata =
     runTestRequest[GraphqlMutationDataFilesMetadata](addFilesAndMetadataJsonFilePrefix)
   val expectedFilesAndMetadataMutationResponse: String => GraphqlMutationDataFilesMetadata =
     getDataFromFile[GraphqlMutationDataFilesMetadata](addFilesAndMetadataJsonFilePrefix)
+
+  val runTestQueryGetAllDescendants: (String, OAuth2BearerToken) => GraphqlQueryDataGetAllDescendants =
+    runTestRequest[GraphqlQueryDataGetAllDescendants](getAllDescendantsJsonFilePrefix)
+  val expectedGetAllDescendantsQueryResponse: String => GraphqlQueryDataGetAllDescendants =
+    getDataFromFile[GraphqlQueryDataGetAllDescendants](getAllDescendantsJsonFilePrefix)
 
   val fixedUuidSource = new FixedUUIDSource()
 
@@ -73,6 +86,27 @@ class FileRouteSpec extends TestContainerUtils with Matchers with TestRequest {
       val expectedResponse = expectedFilesAndMetadataMutationResponse("data_all")
       val response = runTestMutationFileMetadata("mutation_alldata_3", validUserToken())
       expectedResponse.data.get.addFilesAndMetadata should equal(response.data.get.addFilesAndMetadata)
+  }
+
+  "getAllDescendants" should "return all descendants and metadata of the given parent ids" in withContainers {
+    case container: PostgreSQLContainer =>
+      val consignmentId = UUID.fromString("1cd5e07a-34c8-4751-8e81-98edd17d1729")
+      val folderId: String = "7b19b272-d4d1-4d77-bf25-511dc6489d12"
+      val subFolderId: String = "42910a85-85c3-40c3-888f-32f697bfadb6"
+      val fileOneId = "e7ba59c9-5b8b-4029-9f27-2d03957463ad"
+      val fileTwoId = "8d9e6de0-e92f-4fef-b8e9-201322937c9b"
+      val fileThreeId = "9757f402-ee1a-43a2-ae2a-81a9ea9729b9"
+      val utils = TestUtils(container.database)
+      utils.createConsignment(consignmentId, userId, fixedSeriesId, "TEST-TDR-2021-MTB")
+      utils.createFile(UUID.fromString(folderId), consignmentId, "Folder", "folderName", None)
+      utils.createFile(UUID.fromString(subFolderId), consignmentId, "Folder", "subFolderName", Some(UUID.fromString(folderId)))
+      utils.createFile(UUID.fromString(fileOneId), consignmentId, fileName = "fileOneName", parentId = Some(UUID.fromString(folderId)))
+      utils.createFile(UUID.fromString(fileTwoId), consignmentId, fileName = "fileTwoName", parentId = Some(UUID.fromString(folderId)))
+      utils.createFile(UUID.fromString(fileThreeId), consignmentId, fileName = "fileThreeName", parentId = Some(UUID.fromString(subFolderId)))
+
+      val expectedResponse = expectedGetAllDescendantsQueryResponse("data_all")
+      val response = runTestQueryGetAllDescendants("query_all", validUserToken())
+      expectedResponse.data.get.getAllDescendants should equal(response.data.get.getAllDescendants)
   }
 
   def getFileNameAndOriginalPathMatch(fileId: UUID, utils: TestUtils): Option[FileNameAndPath] = {
