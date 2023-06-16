e0f7e9c9c5462a0620165dc9edea0620394bcedf
thanhz <tri.lam21@outlook.com>
2023-06-16 15:11:06 +0100

    update series consignment status

--- src/main/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentService.scala
@@ -117,7 +117,7 @@ class ConsignmentService(
   }
 
   def updateSeriesIdOfConsignment(updateConsignmentSeriesIdInput: UpdateConsignmentSeriesIdInput): Future[Int] = {
-    addSeriesStatus(updateConsignmentSeriesIdInput.consignmentId)
+    consignmentStatusRepository.updateConsignmentStatus(updateConsignmentSeriesIdInput.consignmentId, "Series", Completed, Timestamp.from(timeSource.now))
     consignmentRepository.updateSeriesIdOfConsignment(updateConsignmentSeriesIdInput)
   }
 
