f97d4b33d0ac7b742d33a6bfd7daa50a97ac318e
TomJKing <tom.king@nationalarchives.gov.uk>
2023-07-17 14:11:55 +0100

    Use DB generated 'file status id'

--- src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/FileStatusRepository.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/FileStatusRepository.scala
@@ -11,8 +11,13 @@ class FileStatusRepository(db: Database) {
   private val insertQuery = Filestatus returning Filestatus.map(_.filestatusid) into
     ((fileStatus, filestatusid) => fileStatus.copy(filestatusid = filestatusid))
 
+  private val insertQueryV2 =
+    Filestatus.map(t => (t.fileid, t.statustype, t.value, t.createddatetime)) returning Filestatus.map(_.filestatusid) into ((fileStatus, generatedFileStatusId) =>
+      FilestatusRow(generatedFileStatusId, fileStatus._1, fileStatus._2, fileStatus._3, fileStatus._4)
+    )
+
   def addFileStatuses(fileStatusRows: List[FilestatusRow]): Future[Seq[Tables.FilestatusRow]] = {
-    db.run(insertQuery ++= fileStatusRows)
+    db.run(insertQueryV2 ++= fileStatusRows.map(r => (r.fileid, r.statustype, r.value, r.createddatetime)))
   }
 
   def getFileStatus(consignmentId: UUID, statusTypes: Set[String], selectedFileIds: Option[Set[UUID]] = None): Future[Seq[FilestatusRow]] = {
@@ -35,3 +40,17 @@ class FileStatusRepository(db: Database) {
     db.run(query)
   }
 }
+
+//class X(tag: Tag) extends Table[Filestatus](tag, "Filestatus") {
+//  def fileId = column[UUID]("fileid", O.PrimaryKey, O.AutoInc)
+//
+//  def name = column[String]("name")
+//  override def * : ProvenShape[nationalarchives.Tables.Filestatus] = fileId ~ name
+//}
+
+//object X extends Table[(UUID, UUID, String, String, java.sql.Timestamp)]("filestatus") {
+//  def id = column[UUID]("id", O.PrimaryKey, O.AutoInc)
+//  def name = column[String]("name")
+//
+//  def * = id ~ name // Note: Just a simple projection, not using .? etc
+//}
