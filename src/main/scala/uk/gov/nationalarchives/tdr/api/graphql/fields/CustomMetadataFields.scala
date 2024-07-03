f70e0b9cb6ba5b5b879abf3938d2ddff89d7f339
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-07-03 10:27:38 +0100

    pr comment

--- src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/CustomMetadataFields.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/CustomMetadataFields.scala
@@ -1,13 +1,12 @@
 package uk.gov.nationalarchives.tdr.api.graphql.fields
 
 import io.circe.generic.auto._
-import sangria.marshalling.circe._
 import sangria.macros.derive.{deriveEnumType, deriveObjectType}
+import sangria.marshalling.circe._
 import sangria.schema.{Argument, EnumType, Field, ListType, ObjectType, fields}
 import uk.gov.nationalarchives.tdr.api.auth.ValidateUserHasAccessToConsignment
 import uk.gov.nationalarchives.tdr.api.graphql.ConsignmentApiContext
 import uk.gov.nationalarchives.tdr.api.graphql.fields.FieldTypes.UuidType
-import uk.gov.nationalarchives.tdr.api.graphql.fields.DataTypeFields
 
 import java.util.UUID
 
