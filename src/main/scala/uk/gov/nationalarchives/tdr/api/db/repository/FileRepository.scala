55b2c08740eb822a3831868f1313ff1c36b5574b
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-01-30 09:10:47 +0000

    trying to get tests to work

--- src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/FileRepository.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/FileRepository.scala
@@ -119,7 +119,8 @@ class FileRepository(db: JdbcBackend#Database)(implicit val executionContext: Ex
              f."FileName",
              f."ParentId"::text
             FROM "File" f INNER JOIN children c ON c."FileId"::text = f."ParentId"::text
-        ) SELECT * FROM children;""".as[FileRow]
+        ) SELECT * FROM children;""".stripMargin
+    val sql = sql"""$sqlString""".as[FileRow]
     db.run(sql)
   }
 
