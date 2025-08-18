a5d7a6d9355382b19a1f63576fc2883a79d1854f
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2025-08-18 10:20:45 +0100

    Add to consignment status service

--- src/test/scala/uk/gov/nationalarchives/tdr/api/service/FileServiceSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/service/FileServiceSpec.scala
@@ -47,7 +47,7 @@ class FileServiceSpec extends AnyFlatSpec with MockitoSugar with Matchers with S
   val antivirusMetadataRepositoryMock: AntivirusMetadataRepository = mock[AntivirusMetadataRepository]
   val referenceGeneratorServiceMock: ReferenceGeneratorService = mock[ReferenceGeneratorService]
   val fileStatusServiceMock: FileStatusService = mock[FileStatusService]
-  val consignmentStatusService = new ConsignmentStatusService(consignmentStatusRepositoryMock, uuidSource, FixedTimeSource)
+  val consignmentStatusService = new ConsignmentStatusService(consignmentStatusRepositoryMock, metadataReviewLogRepositoryMock, uuidSource, FixedTimeSource)
   val fileMetadataService = new FileMetadataService(fileMetadataRepositoryMock)
   val queriedFileFieldsWithoutOriginalPath: QueriedFileFields = QueriedFileFields()
 
